31de7a25165b4a6bcc84c9eeebab1f211f1f7289
Brian Jerez <35076446+BrianJerez@users.noreply.github.com>
2018-05-17 22:34:30 -0500

    Add files via upload

---
+++ chat/script.js
@@ -0,0 +1,16 @@
+function hideIcon(){
+	let element = document.getElementById("chat-icon");
+	element.style.display = "none";
+	let show = document.getElementById("chat-box");
+	show.style.display = "inherit";
+}
+
+
+function hideBox(){
+	let element = document.getElementById("chat-box");
+	element.style.display = "none";
+	let show = document.getElementById("chat-icon");
+	show.style.display = "inherit";
+}
+
+
