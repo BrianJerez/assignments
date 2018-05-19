e71714d29242104e2819b82ac8f3fa17fca31ebe
Brian Jerez <35076446+BrianJerez@users.noreply.github.com>
2018-05-18 23:12:53 -0500

    Add files via upload

--- chat/script.js
+++ chat/script.js
@@ -13,4 +13,36 @@ function hideBox(){
 	show.style.display = "inherit";
 }
 
+function createJson(){
+	let element = {
+			message : document.getElementById("area-box").value
+	};
+	return JSON.stringify(element);
+}
+
+$("#area-box").keypress(function(e){
+		if(e.which == 13 || e.keyCode == 13){
+			$.ajax({ 
+					url: "https://api.mlab.com/api/1/databases/demofer/collections/chat?apiKey=WfkQziF9geAVqeiUGnu15k8upTw8mMfR", 
+					data: createJson(), 
+					type: "POST", 
+					contentType: "application/json"
+			});
+			if(e.preventDefault) e.preventDefault();
+			document.getElementById("area-box").value = "";
+
+		}
+});
+
+$.get("https://api.mlab.com/api/1/databases/demofer/collections/chat?apiKey=WfkQziF9geAVqeiUGnu15k8upTw8mMfR", function(data){
+		$(".messageBox").text("");
+		$(".messageBox").append("<ul>");
+		for(let item in data)
+		{
+			$(".messageBox").append("<li>" + data[item].message +"</li>");
+		}
+		$(".messageBox").append("</ul>");
+
+		setTimeout($.get(this), 4000);
+});
 
